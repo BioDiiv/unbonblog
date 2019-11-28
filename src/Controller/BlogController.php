e845f814362de2c76e7fa6a356172607bea569b7
Julien <julien.rousse@etu.umontpellier.fr>
2019-11-28 17:31:30 +0100

    Formulaire style

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,8 +5,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\PostEntity;
 use \DateTime;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BlogController extends AbstractController
 {
@@ -41,15 +41,20 @@ class BlogController extends AbstractController
      */
     public function addPost(){
         $post = new PostEntity();
-        $post->setTitle('Titre');
-        $post->setUrl('url');
-        $post->setContent('Content here');
+        $post->setTitle('');
+        $post->setUrl('');
+        $post->setContent('');
         $post->setPublished(new DateTime);
 
         $form = $this->createFormBuilder($post)
-                ->add('title', TextType::class)
-                ->add('content', TextType::class)
-                ->add('published', DateType::class)
+                ->add('title', TextType::class, array(
+                                             'attr' => array(
+                                                 'placeholder' => 'Un bon titre',
+                                             )))
+                ->add('content', Textareatype::class,array(
+                                             'attr' => array(
+                                                 'placeholder' => 'Un bon article',
+                                             )))
                 ->getForm();
 
         return $this->render('blog/newPost.html.twig',[
